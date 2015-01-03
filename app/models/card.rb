e0fbbbec8515ab9fa57a3c0ccf4f764bc2f930fd
Bratislav Petrovic <bratislav.petrovic@gmail.com>
2015-01-03 21:00:18 +0100

    how to resize?

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,5 +1,5 @@
 class Card
-  @@SUITS = %w(SPADE DIAMOND HEARD CLUB)
+  @@SUITS = %w(SPADE DIAMOND HEART CLUB)
   @@VALUES = %w(A 2 3 4 5 6 7 8 9 10 J Q K)
   @@UNICODE_SUITS = %w( &#x2660 &#x2666 &#x2764 &#x2663) 
   @@CLASS_STR = %w(ace two three four five six seven eight nine ten jack queen king)
